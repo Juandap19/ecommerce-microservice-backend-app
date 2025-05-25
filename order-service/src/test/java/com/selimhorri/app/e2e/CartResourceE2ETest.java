1fe58c3b8b373ba1d19f004d8dd6a23367753077
Juan David Patiño Zambrano <juanda232405@hotmail.com>
2025-05-24 19:13:27 -0500

    adding tests

--- order-service/src/test/java/com/selimhorri/app/e2e/CartResourceE2ETest.java
+++ order-service/src/test/java/com/selimhorri/app/e2e/CartResourceE2ETest.java
@@ -13,7 +13,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
