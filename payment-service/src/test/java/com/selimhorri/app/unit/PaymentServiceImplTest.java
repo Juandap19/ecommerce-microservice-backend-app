61c207a2c73346db540d2af79b92dba25ebc75a7
Juan David Patiño Zambrano <juanda232405@hotmail.com>
2025-05-26 08:53:58 -0500

    fixing test

--- payment-service/src/test/java/com/selimhorri/app/unit/PaymentServiceImplTest.java
+++ payment-service/src/test/java/com/selimhorri/app/unit/PaymentServiceImplTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-public class TestPaymentServiceImplTest {
+public class PaymentServiceImplTest {
 
     private PaymentRepository paymentRepository;
     private RestTemplate restTemplate;
